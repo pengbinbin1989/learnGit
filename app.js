647b4eb55f96e8572b6f4586830fc9470991620a
pengbinbin <475731530@qq.com>
2015-12-10 17:57:47 +0800

    01 add app.js

---
+++ app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var Stories = require('./routes/mainNav/Stories');
+var Teams = require('./routes/mainNav/Teams');
+var Plug_in = require('./routes/mainNav/Plug-in');
+var Jobs = require('./routes/mainNav/Jobs');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/Stories', Stories);
+app.use('/Teams', Teams);
+app.use('/Plug-in', Plug_in);
+app.use('/Jobs', Jobs);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      status:err.status || 500,
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    status:err.status || 500,
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
